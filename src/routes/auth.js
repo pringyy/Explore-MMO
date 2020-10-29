9595d73387670f25160d55429031f40e65c63d24
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2020-10-29 17:47:26 +0000

    Registration validation added
    Back-end not validates if the details are correct

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,21 +1,36 @@
 const router = require('express').Router();
 const User = require('../model/user');
 
+//VALIDATION
+const Joi = require('@hapi/joi');
 
+const schema  = Joi.object ({
+	username: Joi.string().min(3).required(),
+	email: Joi.string().min(6).required().email(),
+	password: Joi.string().min(6).required()
+});
 
 router.post('/register', async (req,res) => {
 
-	const user = new User({
-		username: req.body.username,
-		email: req.body.email,
-		password: req.body.password
-	});
+	//Validates user before creating a user
+	const {error} = schema.validate(req.body);
+	
+	if (error){
+		res.send(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
+	} else {
 
-	try{
-		const savedUser = await user.save();
-		res.send(savedUser);
-	}catch(err){
-		res.status(400).send(err);
+		const user = new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.password
+		});
+
+		try{
+			const savedUser = await user.save();
+			res.send(savedUser);
+		}catch(err){
+			res.status(400).send(err);
+		}
 	}
 });
 
