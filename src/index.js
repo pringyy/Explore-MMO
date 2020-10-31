b791906df8eda8cb5b48bd1c0228f545ab3d2085
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2020-10-31 18:44:21 +0000

    Bug fixed and front-end started

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const postRoute = require('./routes/posts');
 dotenv.config();
 
 //Connecting to MongoDB database being hosted on https://cloud.mongodb.com/
-mongoose.createConnection(
+mongoose.connect(
     process.env.DB_CONNECT,
     {useNewUrlParser:true, useUnifiedTopology:true},
     () => console.log('successfully connected to database')
@@ -17,11 +17,11 @@ mongoose.createConnection(
 
 
 //Middleware
+app.use(express.static('public'));
 app.use(express.json());
 
 //Create Route Middlewares
 app.use('/api/user', authRoute);
 app.use('/api/posts', postRoute);
-
 
 app.listen(3000, () => console.log('server up and running'));
