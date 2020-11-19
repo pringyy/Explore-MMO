06c9c548315eabcbcc9cbab1813ed7b474c471a7
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2020-11-19 19:06:07 +0000

    Removed react and started new frontend

--- src/backend/index.js
+++ src/backend/index.js
@@ -14,15 +14,49 @@ dotenv.config();
 mongoose.connect(
     process.env.DB_CONNECT,
     {useNewUrlParser:true, useUnifiedTopology:true},
-    () => console.log('successfully connected to database')
 );
+
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+  process.exit(1);
+});
+mongoose.connection.on('connected', function () {
+  console.log('connected to mongo');
+});
+
+// Main routes
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Main routes
+app.get('/register', function (req, res) {
+  res.sendFile(__dirname + '/public/register.html');
+});
+
+
 
 //Middleware
 app.use(express.json());
 app.use(cors()); 
+app.use(express.static(__dirname + '/public'));
 
 //Create Route Middlewares
 app.use('/api/user', authRoute);
 app.use('/api/posts', postRoute);
+
+ 
+// // catch all other routes
+// app.use((req, res, next) => {
+//   res.status(404);
+//   res.json({ message: '404 - Not Found' });
+// });
+
+// // handles errors
+// app.use((err, req, res, next) => {
+//   res.status(err.status || 500);
+//   res.json({ error : err });
+// });
+
 
 app.listen(3000, () => console.log('server up and running'));
