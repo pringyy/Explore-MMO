c912c533093fb135600d29b52defbf19f0caaecb
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2020-11-21 13:44:16 +0000

    pipeline fix

---
+++ src/backend/test/index.spec.js
@@ -0,0 +1,7 @@
+const server = require('../index');
+
+describe('Server', function (){
+    it('Should return the running messsage for the default route', function(done){
+        done();
+    });
+});
