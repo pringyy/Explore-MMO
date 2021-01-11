c28c5c9a2b6bd5f96c20a782b82ae3e8011f2613
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2021-01-11 19:07:30 +0000

    Update game.js

--- src/backend/public/js/game.js
+++ src/backend/public/js/game.js
@@ -111,13 +111,13 @@ class UiScene extends Phaser.Scene {
 
   setupUiElements () {
      //create the score text game object
-     this.scoreText = this.add.text(35, 8, 'Coins: 10', { fontSize: '24px', fill: '#fff' });
+     this.scoreText = this.add.text(35, 8, 'Coins left: 10', { fontSize: '24px', fill: '#fff' });
      this.timeText = this.add.text(35, 50, 'Time left: ', { fontSize: '24px', fill: '#fff' });
   };
 
   setupEvents(){
     this.gameScene.events.on('updateScore', (score) => {
-      this.scoreText.setText(`Coins: ${score}`);
+      this.scoreText.setText(`Coins left: ${score}`);
     })
   };
 };
