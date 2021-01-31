807c87bb9f852fe6b8027dd8b512778252e58f5c
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2021-01-31 22:03:32 +0000

    misc change

--- src/backend/public/js/scenes/questScenes/quest1Ui.js
+++ src/backend/public/js/scenes/questScenes/quest1Ui.js
@@ -14,8 +14,8 @@ class quest1Ui extends Phaser.Scene {
     
         setupUiElements () {
             //create the score text game object
-          //  this.rectangle1 = this.add.rectangle(this.scale.width/2, this.scale.height/2, this.scale.width, this.scale.height,'#000000');
-            //this.rectangle1.setOrigin(0.5);
+            this.rectangle1 = this.add.rectangle(this.scale.width/2, this.scale.height/2, this.scale.width, this.scale.height,'#000000');
+            this.rectangle1.setOrigin(0.5);
             this.titleText = this.add.text(this.scale.width/2, this.scale.height/2 * 0.7, 'You have been blind folded.', { fontSize: '32px', fill: '#fff' });
             this.helpText = this.add.text(this.scale.width/2, this.scale.height/2 * 1, 'Objective: complete the maze ', { fontSize: '16px', fill: '#fff' });
             this.helpText2 = this.add.text(this.scale.width/2, this.scale.height/2 * 1.1, '', { fontSize: '16px', fill: '#fff' });
