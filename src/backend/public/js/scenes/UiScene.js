1c89de70e93509a07c67e873d129141c0d294f85
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2021-02-03 13:00:24 +0000

    Update UiScene.js

--- src/backend/public/js/scenes/UiScene.js
+++ src/backend/public/js/scenes/UiScene.js
@@ -14,20 +14,20 @@ class UiScene extends Phaser.Scene {
   
       
   
-
-    setupUiElements () {   
-        this.locationText = this.add.text(this.scale.width/2, this.scale.height*0.97 , 'You are currently in the Spawn', { backgroundColor: '	rgb(0, 0, 0)', fontSize: '14px', fill: '#fff' }).setOrigin(0.5).alpha = 0.7;
-        this.progressText = this.add.text(this.scale.width*0.85, this.scale.height*0.97 , numberCompleted, { backgroundColor: '	rgb(0, 0, 0)', fontSize: '15px', fill: '#fff' }).setOrigin(0.5).alpha = 0.7; 
-    };
-
-    setupEvents(){
+    
+      setupUiElements () {   
+         this.locationText = this.add.text(this.scale.width/2, this.scale.height*0.97 , 'You are currently in the Spawn', { backgroundColor: '	rgb(0, 0, 0)', fontSize: '14px', fill: '#fff' }).setOrigin(0.5).alpha = 0.7;
+         this.progressText = this.add.text(this.scale.width*0.85, this.scale.height*0.97 , numberCompleted, { backgroundColor: '	rgb(0, 0, 0)', fontSize: '15px', fill: '#fff' }).setOrigin(0.5).alpha = 0.7; 
+      };
+    
+      setupEvents(){
         this.gameScene.events.on('updateLocation', (location) => {
             this.locationText.setText(`You are currently ${location}`);
-        });
+         })
 
-        this.gameScene.events.on('progress', (number) => {
+         this.gameScene.events.on('progress', (number) => {
             this.progressText.setText(`${number}/5 Quests Completed`);
-        });
+         })
     };
 };
     
