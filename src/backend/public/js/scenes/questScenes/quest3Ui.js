89948525033d823501a0673e039979e8033510a3
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2021-02-10 17:32:08 +0000

    misc changes

--- src/backend/public/js/scenes/questScenes/quest3Ui.js
+++ src/backend/public/js/scenes/questScenes/quest3Ui.js
@@ -16,7 +16,7 @@ class quest3Ui extends Phaser.Scene {
 
     setupUiElements () {
        //create the score text game object
-       this.objText = this.add.text(35, 8, 'Objective: find the sword at the top of the mountain', { backgroundColor: '	rgb(0, 0, 0)', fontSize: '16px', fill: '#fff' });this.objText.alpha = 0.7;
+       this.objText = this.add.text(35, 8, 'Objective: find the hat at the top of the mountain', { backgroundColor: '	rgb(0, 0, 0)', fontSize: '16px', fill: '#fff' });this.objText.alpha = 0.7;
        this.quitButton = new UiButton(this, this.scale.width*0.86, this.scale.height*0.05, 'button1', 'button2', 'Give up/Respawn', this.endScene.bind(this, 'quest2Ui'));
        this.quitButton.setScale(0.7);    
       };
