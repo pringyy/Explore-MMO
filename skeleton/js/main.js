2cb41203d2c4b73bfa203d1f1b503554e8375096
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2021-03-12 18:56:50 +0000

    Delete skeleton directory

--- skeleton/js/main.js
+++
@@ -1,4 +0,0 @@
-/*I used this tutorial to learn how to do this with Phasher: https://www.dynetisgames.com/2017/03/06/how-to-make-a-multiplayer-online-game-with-phaser-socket-io-and-node-js*/
-var game = new Phaser.Game(24*32, 17*32, Phaser.AUTO, document.getElementById('game'));
-game.state.add('Game',Game);
-game.state.start('Game');
